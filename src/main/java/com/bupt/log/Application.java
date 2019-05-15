80b7f9c8f3b5c3082c73538921a9e81c41798e3f
LH <861561630@qq.com>
2019-05-15 16:16:48 +0800

    测试commit&push

--- src/main/java/com/bupt/log/Application.java
+++ src/main/java/com/bupt/log/Application.java
@@ -2,11 +2,12 @@ package com.bupt.log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//测试github
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("test GitHub");
 		SpringApplication.run(Application.class, args);
 	}
 
