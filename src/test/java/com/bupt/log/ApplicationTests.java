e767f0512d4be98c35307782316eb862abe53f3e
LH <861561630@qq.com>
2019-05-19 10:30:48 +0800

    测试commit&push

--- src/test/java/com/bupt/log/ApplicationTests.java
+++ src/test/java/com/bupt/log/ApplicationTests.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
@@ -33,49 +35,24 @@ public class ApplicationTests {
 	@Autowired
 	public LogService logService;
 	@Test
-	public int contextLoads() throws Exception {
+	public void contextLoads() throws Exception {
 	//logService.insertAll("C:\\Users\\LH\\Desktop\\Other\\201812");
 		//mobilelogService.insertAll("C:\\Users\\LH\\Desktop\\Other\\201812");
-		int min=Integer.MAX_VALUE;
-		int tmpn=0;
-		int a=0;
-		Scanner s=new Scanner(System.in);
-		String ss=s.nextLine();
-		String[] sss=ss.split(" ");
-		int m=Integer.parseInt(sss[0]);
-		int n=Integer.parseInt(sss[1]);
-		ss=s.nextLine();
-		String[] nums=ss.split(" ");
-		Map<Integer,Integer> map=new HashMap<Integer,Integer>();
-		List<Integer> list=new ArrayList<Integer>();
-		for(int i=0;i<m;i++){
-			int tmp= Integer.parseInt(nums[i]);
-			if(map.keySet().contains(tmp))
-				map.put(tmp,map.get(tmp)+1);
-			else{
-				map.put(tmp,1);
-				list.add(tmp);
-			}
+
+	}
+	@Test
+	public void tese(){
+		String[] strings=new String[5];
+		Query query=new Query();
+		String isIP = "\\d+\\.\\d+\\.\\d+\\.\\d+";
+		String isUuid="\\d{}";
+		Pattern pforIP = Pattern.compile(isIP);
+		Matcher m = pforIP.matcher("192.168.0.1");
+		System.out.println(m.find());
+		for (String string:strings){
+			if (m.find())
+				query.addCriteria(Criteria.where("ip").is(string));
+			else if ()
 		}
-		Collections.sort(list);
-		int size=list.size();
-		for (int i=size-1;i>0;i++){
-			int tmpi=i;
-			int tmph=map.get(list.get(i));
-			if (tmph>=n)
-				return 0;
-			else {
-				while(tmph<n){
-					tmpi-=1;
-					tmph+=map.get(tmpi);
-					if(tmph<n)
-						tmpn+=(list.get(i)-list.get(tmpi))*map.get(tmpi);
-					else
-						tmpn+=(list.get(i)-list.get(tmpi))*(n-tmph);
-				}
-			}
-			min=min<tmpn?min:tmph;
-		}
-		return min;
 	}
 }
